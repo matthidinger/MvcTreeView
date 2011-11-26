b8ea01174dc503010bf34b891f306fd5f5e207ab
Matt Hidinger <Matt Hidinger@ZENBOOK.hsd1.mi.comcast.net.>
2011-11-26 15:07:38 -0600

    initial commit

---
+++ MvcTreeView/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MvcTreeView.Models;
+
+namespace MvcTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult FromDatabase()
+        {
+            return View();
+        }
+
+        public ActionResult FromMemory()
+        {
+            var locations = new List<Location>
+                                {
+                                    new Location
+                                        {
+                                            Id = 1,
+                                            Name = "United States",
+                                            ChildLocations =
+                                                {
+                                                    new Location {Id = 2, Name = "Chicago", ChildLocations =
+                                                                                                {
+                                                                                                    new Location { Name = "Rack 1"},
+                                                                                                    new Location { Name = "Rack 2"},
+                                                                                                    new Location { Name = "Rack 3"},
+                                                                                                }},
+                                                    new Location {Id = 2, Name = "Dallas"}
+                                                }
+                                        },
+                                         new Location
+                                        {
+                                            Id = 1,
+                                            Name = "Canada",
+                                            ChildLocations =
+                                                {
+                                                    new Location {Id = 2, Name = "Ontario"},
+                                                    new Location {Id = 2, Name = "Windsor"}
+                                                }
+                                        }
+                                };
+            return View(locations);
+        }
+
+
+    }
+}
